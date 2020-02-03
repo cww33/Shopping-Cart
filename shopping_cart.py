bb06ac8f422aa0d593e02877673559ef51de3400
cww33 <59658864+cww33@users.noreply.github.com>
2020-02-03 08:37:24 -0500

    Append List and Print when input Done

--- shopping_cart.py
+++ shopping_cart.py
@@ -30,47 +30,23 @@ products = [
 
 # TODO: write some Python code here to produce the desired output
 
+userreceipt =[]
 
 while True:
     userproduct= input("Please input the product identifier: ")
+    userreceipt.append(userproduct)
     if userproduct == ("DONE"):
+        print (userreceipt)
         break
-    elif userproduct != range(1, 20, 1):
-        print ("Ensure it is a valid product identifier")
+    #elif userproduct != range(1, 20):
+       # print ("Ensure it is a valid product identifier")
+   # else:
+       # userreceipt.append(userproduct)
+    
+
+    #print (userreceipt)
 
 
 
 
-#userreceipt= []
-#
-#userproducts== float(input("Please input a product identifier: "))
-#if userproduct == ("done").upper():
-#    break
-#elif userproducts<1 or userproducts>20 or userproducts!= int:
-#        print("Hey, are you sure thatx product identifier is correct? Please try again!")
-#else:
-#    userproducts== float(input("Please input a product identifier"))
-#    for userproducts in range(1,20):
-#        userproducts= userreceipt.append() #programiz.com
-#        userproducts= float(input("Please input a product identifier")
-#
-##if userproducts<1 or userproducts>20 or userproducts != int:
-# #       print("Hey, are you sure that product identifier is correct? Please try again!")    
-#  #  elif userproducts ==("done").upper()
-#   #     break
-#
-##for i in userreceipt[]:
-# #   print(userreceipt[0]) 
-#    
-#
-#
-##if userproducts != [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]:
-#    #print("Hey, are you sure that product identifier is correct? Please try again!")
-#    #userproducts input("Please input a product identifier")
-#    #elif userproducts = ("done"):
-#       # print ("no inputs detected")
-#        #break
-#
-#
-##for userproducts = input("Please input product identifier")
-#    #while userproducts>=1 and userproducts <=20
+
