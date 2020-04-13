c68034bcd715dfdcf48d519b0f12c667cfd7d47e
cww33 <59658864+cww33@users.noreply.github.com>
2020-04-13 17:14:47 -0400

    Pull Request 2

--- my_test.py
+++ my_test.py
@@ -1,20 +1,21 @@
 from shopping_cart import to_usd
 from shopping_cart import taxtotal
+from shopping_cart import total
 from shopping_cart import subtotal
 from shopping_cart import taxpercentage
 
-subtotal=100
-taxtotal=8.75
+#subtotal=100
+#taxtotal=8.75
 
 def test_to_usd():
     result= to_usd(73498.82 )
     assert result == " $73,498.82"
 
 def test_taxtotal():
-    result=subtotal*taxpercentage
-    assert result == 8.75
+    result= taxtotal
+    assert result == subtotal*taxpercentage
 
 def test_total():
     result= subtotal+taxtotal
-    assert result == 108.75
+    assert result == total
 
