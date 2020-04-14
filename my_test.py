54f592da9b1f6d09a654d9670ea76cd01bef4cca
cww33 <59658864+cww33@users.noreply.github.com>
2020-04-14 18:12:46 -0400

    New tests to add for testing USD

--- my_test.py
+++ my_test.py
@@ -1,20 +1,22 @@
 from shopping_cart import to_usd
 from shopping_cart import taxtotal
+from shopping_cart import total
 from shopping_cart import subtotal
 from shopping_cart import taxpercentage
 
-subtotal=100
-taxtotal=8.75
 
 def test_to_usd():
-    result= to_usd(123456.78)
-    assert result == " $123,456.78"
-
+    result= to_usd(73498.82 )
+    assert result == " $73,498.82"
+    assert to_usd(9.9) == " $9.90"
+   
 def test_taxtotal():
-    result=subtotal*taxpercentage
-    assert result == 8.75
+    result= taxtotal
+    assert result == subtotal*taxpercentage
 
 def test_total():
     result= subtotal+taxtotal
-    assert result == 108.75
+    assert result == total
+
+
 
